4b41d044e1db9aa834cd0822c4b48f6abea03f01
Bruno Santos <brsantos197@gmail.com>
2024-12-12 08:40:19 -0300

    refactor(entities): atualiza propriedades das entidades Answer e Question
    - Adiciona UniqueEntityId para authorId e questionId em Answer
    - Inclui campos de data (createdAt e updatedAt) em Answer
    - Adiciona UniqueEntityId para authorId e bestAnswerId em Question
    - Inclui campos de data (createdAt e updatedAt) em Question
    - Ajusta a ordem das propriedades em ambas as entidades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,10 +1,14 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 import { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
+  authorId: UniqueEntityId
+  bestAnswerId?: UniqueEntityId
   title: string
-  slug: Slug
   content: string
-  authorId: string
+  slug: Slug
+  createdAt: Date
+  updatedAt?: Date
 }
 export class Question extends Entity<QuestionProps> {}
