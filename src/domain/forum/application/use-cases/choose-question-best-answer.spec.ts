48147c16149d6774e188ff61a87c27a2f4188436
Bruno Santos <brsantos197@gmail.com>
2024-12-13 02:45:45 -0300

    feat: adiciona caso de uso para escolher melhor resposta da pergunta
    - Implementa ChooseQuestionBestAnswerUseCase
    - Adiciona testes para o novo caso de uso
    - Verifica permissões do autor da pergunta
    - Atualiza a entidade Question com a melhor resposta escolhida

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
@@ -0,0 +1,64 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { DeleteAnswerUseCase } from './delete-answer'
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { ChooseQuestionBestAnswerUseCase } from './choose-question-best-answer'
+import { makeQuestion } from 'test/factories/make-question'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: ChooseQuestionBestAnswerUseCase
+
+describe('Choose Question Best Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new ChooseQuestionBestAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryQuestionsRepository,
+    )
+  })
+  it('should be able choose the best answer', async () => {
+    const question = makeQuestion()
+
+    const awnser = makeAnswer({
+      questionId: question.id,
+    })
+
+    await inMemoryQuestionsRepository.create(question)
+    await inMemoryAnswersRepository.create(awnser)
+
+    await sut.execute({
+      answerId: awnser.id.toString(),
+      authorId: question.authorId.toString(),
+    })
+
+    expect(inMemoryQuestionsRepository.items[0].bestAnswerId).toEqual(awnser.id)
+  })
+
+  it('should not be able to choose another user question best answer', async () => {
+    const question = makeQuestion({
+      authorId: new UniqueEntityId('author-1'),
+    })
+
+    const awnser = makeAnswer({
+      questionId: question.id,
+    })
+
+    await inMemoryQuestionsRepository.create(question)
+    await inMemoryAnswersRepository.create(awnser)
+
+    await sut.execute({
+      answerId: awnser.id.toString(),
+      authorId: question.authorId.toString(),
+    })
+
+    await expect(() => {
+      return sut.execute({
+        answerId: awnser.id.toString(),
+        authorId: 'author-2',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
